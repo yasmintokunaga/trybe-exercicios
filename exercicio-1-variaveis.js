5e83ba6f6544a579aa7af4b59cb2bd240ae7f595
yasmintokunaga <yasmintkng@gmail.com>
2022-11-24 11:51:55 -0300

    Resolvendo exercicios dia 1 Java Script

---
+++ exercicio-1-variaveis.js
@@ -0,0 +1,15 @@
+// testando variáveis
+
+const myName = 'Yasmin';
+const birthCity = 'São Paulo';
+let birthYear = 1995;
+
+console.log(myName+"-"+birthCity+"-"+birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = Guarulhos;
+
+// console.log(birthCity); não é possível alterar uma const
---
+++ exercicio-2-tipagem.js
@@ -0,0 +1,15 @@
+// utilizando typeof para descobrir qual é o tipo de variável
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientEmail);
+console.log(typeof patientInfo);
+console.log(typeof firstName);
