0df5ff27be2cbd1276cff367915b4a71a8e02aaf
yasmintokunaga <yasmintkng@gmail.com>
2022-11-24 13:25:53 -0300

    exercicio-10-pratica

---
+++ exercicio-10-pratica.js
@@ -0,0 +1,11 @@
+// Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+let value = -5;
+
+if (value > 0) {
+    console.log('positive');
+} else if ( value < 0){
+    console.log('negative');
+} else {
+    console.log('zero');
+}
