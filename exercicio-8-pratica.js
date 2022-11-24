4cfabfdb923f29a20b77861c2bab826f933a5dda
yasmintokunaga <yasmintkng@gmail.com>
2022-11-24 13:05:22 -0300

    exercicio - 8 pratica

---
+++ exercicio-8-pratica.js
@@ -0,0 +1,10 @@
+// Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
+
+const num1 = 5;
+const num2 = 4;
+
+if (num1 > num2) {
+    console.log('O maior numero é: '+num1);
+} else {
+    console.log('O maior numero é: '+num2);
+}
