892de18ee11647f7ef970648985cec56c214bb10
yasmintokunaga <yasmintkng@gmail.com>
2022-11-29 15:23:55 -0300

    exercicio-3-fixando-aprendizados.js

---
+++ exercicio-2-fixando-aprendizados.js
@@ -0,0 +1,9 @@
+// Utilizando os conceitos de Algoritmos e Lógica de Programação, calcule a soma de todos os números de 1 a 100. O resultado será:
+
+let soma = 0;
+
+for (let index = 1; index <=100; index += 1) {
+    soma += index;
+}
+
+console.log(soma);
