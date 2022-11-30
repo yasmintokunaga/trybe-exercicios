9bf8373667d37a42132a0e7351a9fd15cd09ad10
yasmintokunaga <yasmintkng@gmail.com>
2022-11-30 15:59:47 -0300

    exercicio-1-objetos.js

---
+++ exercicio-1-objetos.js
@@ -0,0 +1,14 @@
+//Objetos
+
+let diasDaSemana = {
+    1: 'domingo',
+    2: 'segunda',
+    3: 'terça',
+    4: 'quarta',
+    5: 'quinta',
+    6: 'sexta',
+    7: 'sábado',
+  };
+  
+//   console.log(diasDaSemana.1); // SyntaxError: Unexpected number
+  console.log(diasDaSemana['1']); // domingo
