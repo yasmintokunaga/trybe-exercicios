79edad9121f70de9d7ca00dd63c16a98c8c1ce51
yasmintokunaga <yasmintkng@gmail.com>
2022-12-01 18:36:45 -0300

    exercicio-12-pratica.js

---
+++ exercicio-12-pratica.js
@@ -0,0 +1,20 @@
+// Crie uma função que ligue e desligue um motor de um carro.
+
+// Crie uma variável para armazenar o valor de status do seu motor (lembre-se de armazenar o tipo de variável da forma correta);
+
+// Crie a função ligarDesligar utilizando arrow functions. Ela não receberá nenhum parâmetro;
+
+// Crie a lógica para ligar e desligar o motor (a lógica deverá utilizar uma condicional para ligar e desligar o motor);
+
+// Imprima no terminal “O motor está ligado“ e “O motor está desligado“ (lembre-se de utilizar o template literals nesse momento).
+
+// Bônus (opcional): tente fazer o mesmo exercício utilizando ternary operator.
+
+let estado = 'ligado';
+
+const ligarDesligar = () => estado === 'ligado' ? estado ='desligado' : estado = 'ligado';
+
+console.log(`O motor está ${ligarDesligar()}`);
+console.log(`O motor está ${ligarDesligar()}`);
+
+
