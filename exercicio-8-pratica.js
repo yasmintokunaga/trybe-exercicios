322bcbeb24a09c380a98cdafda64a4d440f4e33e
yasmintokunaga <yasmintkng@gmail.com>
2022-12-01 17:29:19 -0300

    exercicio-8-pratica.js

---
+++ exercicio-8-pratica.js
@@ -0,0 +1,11 @@
+// Modifique a estrutura das funções a seguir para que elas sejam arrow functions;
+
+// 🚀 Transforme a função numeroAleatorio em uma arrow function;
+
+// function numeroAleatorio() {
+//     return Math.random()
+//   }
+
+  const numeroAleatorio = () => Math.random()*100;
+
+  console.log(numeroAleatorio());
