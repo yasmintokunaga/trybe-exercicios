f627d55a53708b12e552a05dd9ece358843dcead
yasmintokunaga <yasmintkng@gmail.com>
2022-12-03 11:41:21 -0300

    Ajustando requisito 3 para arrow funciotn

--- projeto-requisito-3.js
+++ projeto-requisito-3.js
@@ -1,8 +1,6 @@
 // Implemente a função concatName que recebe um array de string e retorna uma string com último e primeiro item
 
-function concatName (arrayName) {
-    return `${arrayName[arrayName.length - 1]}, ${arrayName[0]}`;
-}
+const concatName = (arrayName) => `${arrayName[arrayName.length - 1]}, ${arrayName[0]}`;
 
 let nameTest = ['Yasmin', 'Souza', 'Tokunaga'];
 console.log(concatName(nameTest));
