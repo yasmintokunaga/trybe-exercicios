94c330c6da4f3077f393d2e55fc48e0de2edc5f1
yasmintokunaga <yasmintkng@gmail.com>
2022-12-03 19:39:45 -0300

    Refatoração exercicio-5-projeto

--- projeto-requisito-5.js
+++ projeto-requisito-5.js
@@ -1,19 +1,16 @@
 // Implemente a função highestCount que deverá retornar a quantidade de vezes que o maior número se repete ao receber um array de números
 
-const highestCount = (arrayNumbers) => {
-    
+const highestCount = (arrayNumbers) => {    
     let identifyHighestNumber = arrayNumbers[0];
-    for (let index = 1; index < arrayNumbers.length; index += 1) {
-        identifyHighestNumber = arrayNumbers[index] > identifyHighestNumber ? arrayNumbers[index] : identifyHighestNumber;
+    for (value of arrayNumbers) {
+        identifyHighestNumber = value > identifyHighestNumber ? value : identifyHighestNumber;
     }
-
     let count = 0;
-    for (let index = 0; index < arrayNumbers.length; index += 1) {    
-        count = arrayNumbers[index] === identifyHighestNumber ? count += 1 : count;
+    for (value of arrayNumbers) {    
+        count = value === identifyHighestNumber ? count += 1 : count;
     }
     return count;
 }
 
 let arrayTest = [0, 0, 0];
-
 console.log(highestCount(arrayTest));
