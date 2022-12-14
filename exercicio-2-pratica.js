1928beafa3a67c6e4f131e05af1cdf823afb6c04
yasmintokunaga <yasmintkng@gmail.com>
2022-12-14 09:48:38 -0300

    Feito parte 1 exercicio2

--- exercicio-2-pratica.js
+++ exercicio-2-pratica.js
@@ -15,3 +15,31 @@ const createDaysOfTheWeek = () => {
   
   // Escreva seu código abaixo.
   
+const createDaysOfMonth = () => {
+  const decemberDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const decemberDaysList = document.querySelector('#days');
+  const holidayDecember = [24, 25, 31];
+  const fridayDecember = [4, 11, 18, 25];
+
+
+  for (value of decemberDays) {
+    const dayOfDecember = document.createElement('li');
+    dayOfDecember.className = 'day';
+    dayOfDecember.innerHTML = value;
+
+    decemberDaysList.appendChild(dayOfDecember);
+
+    for(value2 of holidayDecember) {
+      if(value === value2) {
+        dayOfDecember.classList.add('holiday');
+      }
+    }
+
+    for(value3 of fridayDecember) {
+      if(value3 === value) {
+        dayOfDecember.classList.add('friday');
+      }
+    }
+  }
+}
+  createDaysOfMonth();
