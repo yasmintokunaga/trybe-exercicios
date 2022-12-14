7eb8040c7a5504aef29ca412ad20af906843fd5b
yasmintokunaga <yasmintkng@gmail.com>
2022-12-13 22:02:18 -0300

    resolvendo exercicio mario

--- exercicio-aula-mario/script.js
+++ exercicio-aula-mario/script.js
@@ -1,66 +1,60 @@
 window.onload = () => {
-  //CAPTURA DE ELEMENTOS
-  const starBtn = document.getElementById('start-race-btn');
-  const resetBtn = document.getElementById('reset-race-btn');
+  //PEGANDO OS ELEMENTOS
   const player1 = document.getElementById('player1');
-  const player2= document.getElementById('player2');
-  const audioWinner = document.getElementById('audioWinner');
-  
-  const resetCars = () => {
+  const player2 = document.getElementById('player2');
+  const buttonStart = document.getElementById('start-race-btn');
+  const buttonReset = document.getElementById('reset-race-btn');
+  const carSection = document.querySelector('.car-section');
+
+  //INICIALIZANDO/ VOLTANDO A CORRIDA NO MARCO ZERO
+  function resetRun() {
     player1.style.marginLeft = 0;
     player1.style.backgroundImage = 'url(./files/selectPlayer.png)';
     player2.style.marginLeft = 0;
     player2.style.backgroundImage = 'url(./files/selectPlayer.png)';
-  };
-
-  resetCars();
-
-  starBtn.addEventListener('click', () => {
-    player1.style.marginLeft = parseInt(player1.style.marginLeft) + Math.random() * 100 + 'px';
-    player2.style.marginLeft = parseInt(player2.style.marginLeft) + Math.random() * 100 + 'px';
-
-    const player1Win = parseInt(player1.style.marginLeft) > window.innerWidth;
-    const player2Win = parseInt(player2.style.marginLeft) > window.innerWidth;
-
-    if(player1Win) {
-      alert('PLAYER 1 GANHOU! UHUUL!!');
-      resetCars();
-      audioWinner.play();
-    }
-
-    if(player2Win) {
-      alert('PLAYER 2 GANHOU! UHUUL!!');
-      resetCars();
-      audioWinner.play();
-    }
+  }
+  buttonReset.addEventListener('click', () => {
+    resetRun();
   });
 
-  resetBtn.addEventListener('click', resetCars);
+  //ANDANDO COM OS CARRINHOS
+  resetRun();
+  buttonStart.addEventListener('click', () => {
+    player1.style.marginLeft = parseInt(player1.style.marginLeft) + (Math.random() * 100) + 'px';
+    player2.style.marginLeft = parseInt(player2.style.marginLeft) + (Math.random() * 100) + 'px';
+    
+    let player1WinRun = parseInt(player1.style.marginLeft) > window.innerWidth;
+    let player2WinRun = parseInt(player2.style.marginLeft) > window.innerWidth;
 
-  //SELECIONA PLAYER - CARRINHO 
-  const cars = document.getElementsByClassName('car');
-  const alternativas = document.getElementsByClassName('playersImages');
+    if(player1WinRun) {
+      alert('Jogador 1 venceu!! UHHUL!!');
+      resetRun();
+    }
+    if(player2WinRun) {
+      alert('Jogador 2 venceu!! UHHUL!!');
+      resetRun();
+    }
+  });
+  //SELECIONANDO O PLAYER 1 OU PLAYER 2
+  const players = document.getElementsByClassName('car');
 
-  //SELECIONA PLAYER 1 OU PLAYER 2
-  for(let index = 0; index < cars.length; index += 1) {
-    cars[index].addEventListener('click', (event) => {
+  for(let index = 0; index < players.length; index += 1) {
+    players[index].addEventListener('click', () => {
       const selected = document.querySelector('.selected');
       if(selected){
         selected.classList.remove('selected');
       }
-
-      event.target.classList.add('selected');
-    })
+      players[index].classList.add('selected');
+    });
   }
 
-  //SELECIONA O CARRINHO
-  for(let index = 0; index < alternativas.length; index += 1) {
-    alternativas[index].addEventListener('click', (event) => {
+  //SELECIONANDO O PERSONAGEM
+  const pilots = document.getElementsByClassName('playersImages');
+  for(let index = 0; index < pilots.length; index += 1) {
+    pilots[index].addEventListener('click', () => {
       const selected = document.querySelector('.selected');
-      if(selected) {
-        selected.style.backgroundImage = `url(${event.target.src})`;
-        selected.classList.remove('selected');
-      }
+      selected.style.backgroundImage = `url(${pilots[index].src})`;
+      selected.classList.remove('selected');
     })
   }
 
