771f2c19924588d2690b7266f08939dd0e63df74
yasmintokunaga <yasmintkng@gmail.com>
2022-12-14 23:37:38 -0300

    adição de local storage ao exercicio do mario

--- exercicio-aula-mario/script.js
+++ exercicio-aula-mario/script.js
@@ -6,6 +6,23 @@ window.onload = () => {
   const buttonReset = document.getElementById('reset-race-btn');
   const carSection = document.querySelector('.car-section');
 
+  let ps1score = document.getElementById('p1Score');
+  let ps2score = document.getElementById('p2Score');
+
+  if(localStorage === 0) {
+    ps1score.innerHTML = 0;
+    ps2score.innerHTML = 0;
+  } else if(localStorage.getItem('scorePlayer1') === null) {
+    ps1score.innerHTML = 0;
+    ps2score.innerHTML = localStorage.getItem('scorePlayer2');
+  } else if(localStorage.getItem('scorePlayer2') === null) {
+    ps1score.innerHTML = localStorage.getItem('scorePlayer1');
+    ps2score.innerHTML = 0;
+  }  else {
+    ps1score.innerHTML = localStorage.getItem('scorePlayer1');
+    ps2score.innerHTML = localStorage.getItem('scorePlayer2');
+  }
+
   //INICIALIZANDO/ VOLTANDO A CORRIDA NO MARCO ZERO
   function resetRun() {
     player1.style.marginLeft = 0;
@@ -20,18 +37,36 @@ window.onload = () => {
   //ANDANDO COM OS CARRINHOS
   resetRun();
   buttonStart.addEventListener('click', () => {
-    player1.style.marginLeft = parseInt(player1.style.marginLeft) + (Math.random() * 100) + 'px';
-    player2.style.marginLeft = parseInt(player2.style.marginLeft) + (Math.random() * 100) + 'px';
+    player1.style.marginLeft = parseInt(player1.style.marginLeft) + (Math.random() * 300) + 'px';
+    player2.style.marginLeft = parseInt(player2.style.marginLeft) + (Math.random() * 300) + 'px';
     
     let player1WinRun = parseInt(player1.style.marginLeft) > window.innerWidth;
     let player2WinRun = parseInt(player2.style.marginLeft) > window.innerWidth;
 
     if(player1WinRun) {
       alert('Jogador 1 venceu!! UHHUL!!');
+      if(localStorage.getItem('scorePlayer1') === null) {
+        localStorage.setItem('scorePlayer1', 1);
+        // ps1score.innerHTML = localStorage.getItem('scorePlayer1');
+      } else {
+        let player1Score = parseInt(localStorage.getItem('scorePlayer1'));
+        localStorage.setItem('scorePlayer1', player1Score + 1);
+        // ps1score.innerHTML = localStorage.getItem('scorePlayer1');
+      }
+      ps1score.innerHTML = localStorage.getItem('scorePlayer1');
       resetRun();
     }
     if(player2WinRun) {
       alert('Jogador 2 venceu!! UHHUL!!');
+      if(localStorage.getItem('scorePlayer2') === null) {
+        localStorage.setItem('scorePlayer2', 1);
+        // ps1score.innerHTML = localStorage.getItem('scorePlayer2');
+      } else {
+        let player2Score = parseInt(localStorage.getItem('scorePlayer1'));
+        localStorage.setItem('scorePlayer2', player2Score + 1);
+        // ps1score.innerHTML = localStorage.getItem('scorePlayer2');
+      }
+      ps2score.innerHTML = localStorage.getItem('scorePlayer2');
       resetRun();
     }
   });
