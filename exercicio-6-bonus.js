c98ddf1d26ec2b61c5f8a3b1726a286d57e969dc
yasmintokunaga <yasmintkng@gmail.com>
2023-01-10 13:18:59 -0300

    exercicio-6-bonus

---
+++ exercicio-6-bonus.js
@@ -0,0 +1,48 @@
+var picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'DD/MM/YYYY',
+  toString(date, format) {
+    // you should do formatting based on the passed format,
+    // but we will just return 'D/M/YYYY' for simplicity
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    // dateString is the result of `toString` method
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+});
+
+import JustValidate from 'just-validate';
+
+const validate = new JustValidate('#form');
+
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
