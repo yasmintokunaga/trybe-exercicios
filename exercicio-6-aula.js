df12ca10d7388f95d2a548efe29a1c4f82c84562
yasmintokunaga <yasmintkng@gmail.com>
2023-01-16 15:28:57 -0300

    exercicio-6-aula.js

---
+++ exercicio-6-aula.js
@@ -0,0 +1,34 @@
+const student1 = {
+  html: 'Muito Bom',
+  css: 'Bom',
+  javascript: 'Ótimo',
+  softskills: 'Ótimo',
+};
+
+const student2 = {
+  html: 'Bom',
+  css: 'Ótimo',
+  javascript: 'Ruim',
+  softskills: 'Ótimo',
+  git: 'Bom', // chave adicionada
+};
+
+// const listarHabilidades = (objeto) => {
+//   const arrayKeys = Object.keys(objeto);
+
+//   for (let index = 0; index < arrayKeys.length; index += 1) {
+//     console.log(`${arrayKeys[index]}, Nível: ${objeto[arrayKeys[index]]}`);
+//   }
+// };
+
+const listarHabilidades = (objeto) => {
+  const arrayKeys = Object.keys(objeto);
+  const arrayValues = Object.values(objeto);
+
+  for (let index = 0; index < arrayKeys.length; index += 1) {
+    console.log(`${arrayKeys[index]}, Nível: ${arrayValues[index]}`);
+  }
+}
+
+listarHabilidades(student1);
+listarHabilidades(student2);
