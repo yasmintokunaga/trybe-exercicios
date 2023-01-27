32269e6b03d48d93caf25f3b68a22e1de9e5da8f
yasmintokunaga <yasmintkng@gmail.com>
2023-01-27 18:04:49 -0300

    exercicio-5

---
+++ exercicio-5-pratica.js
@@ -0,0 +1,22 @@
+const myList = [5, 2, 3];
+
+const swap = ([a, b, c]) => [c, b, a];
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([name, brand, year]) => ({ name, brand, year });
+
+// escreva greet abaixo
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
+
+const greet = (name, msg = 'Hi') => `${msg} ${name}`;
+
+// console.log(greet('John')); // 'Hi John'
+// console.log(greet('John', 'Good morning')); // 'Good morning John'
+// console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
