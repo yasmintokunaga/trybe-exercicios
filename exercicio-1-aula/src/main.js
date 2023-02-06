3fa425e0358b48b71f4a6bb903824abe370e2346
yasmintokunaga <yasmintkng@gmail.com>
2023-02-06 14:13:05 -0300

    Adicionando botão copy

--- exercicio-1-aula/src/main.js
+++ exercicio-1-aula/src/main.js
@@ -1,14 +1,22 @@
 // console.log('Hello Wolrd');
 
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css'
 
 // console.log(nanoid());
-
-const passwordBtnEl = document.querySelector('button');
+const passwordBtnEl = document.getElementById('btn-generate');
 const displayPasswordEl = document.querySelector('h2');
+const copyButton = document.getElementById('btn-copy');
 
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
 });
+
+copyButton.addEventListener('click', () => {
+  const copyPassword =   displayPasswordEl.innerText;
+  copy(copyPassword);
+  alert('senha copiada')
+}
+);
